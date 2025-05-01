c8e63b2fe4d1e2a44b6e8739fb644f4caa099aa0
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2025-05-01 17:12:04 -0600

    Built a working app

--- simple_project_button/lib/main.dart
+++ simple_project_button/lib/main.dart
@@ -1,122 +1,200 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class ChallengeDescription extends StatelessWidget {
+  final String text;
+  final String? linkText;
+  final String? url;
+  final String? imageUrl;      // for online image
+  final String? localImage;    // for local image (e.g. "assets/images/my_image.png")
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const ChallengeDescription({
+    required this.text,
+    this.linkText,
+    this.url,
+    this.imageUrl,
+    this.localImage,
+    Key? key,
+  }) : super(key: key);
 
-  // This widget is the root of your application.
+  void _launchURL(BuildContext context) async {
+    if (url == null) return;
+    final uri = Uri.parse(url!);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Could not launch URL")),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(text, style: Theme.of(context).textTheme.bodyMedium),
+        if (url != null && linkText != null)
+          GestureDetector(
+            onTap: () => _launchURL(context),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                linkText!,
+                style: TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+        if (imageUrl != null)
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Image.network(imageUrl!),
+          ),
+        if (localImage != null)
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Image.asset(localImage!),
+          ),
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+void main() {
+  runApp(ChallengeApp());
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+
+
+class Challenge {
+  final String title;
+  final Widget description;
+  
+  bool isCompleted;
+
+  Challenge({required this.title, required this.description, this.isCompleted = false });
+}
+
+class ChallengeApp extends StatefulWidget {
+  @override
+  _ChallengeAppState createState() => _ChallengeAppState();
+}
+
+class _ChallengeAppState extends State<ChallengeApp> {
+  List<Challenge> challenges = [
+    Challenge(title: "Go to the BYU-I letters", description:
+      ChallengeDescription(
+        text: 'Take a picture of yourself next to the big, blue BYU-I letters',
+        linkText: "Learn more here",
+        url: "https://www.youtube.com/watch?v=QREUmhrZ95c",
+        localImage: "assets/IMG_3543.jpg",
+        ), 
+      ),
+    Challenge(title: "Go to one of your professors offices", description: 
+      ChallengeDescription(text: 'Find your proffessors'),
+      ),
+    Challenge(title: "Go to president's dinner", description: 
+      ChallengeDescription(text: "Try going to president's dinner Thursday night!",
+      localImage: 'assets/kingjoey.jpg',
+      )
+    ),
+  ];
+
+  void toggleCompletion(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = _counter +10 ;
+      challenges[index].isCompleted = !challenges[index].isCompleted;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      title: 'Challenge Tracker',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Challenge card!'),
+        ),
+        body: ListView.builder(
+          itemCount: challenges.length,
+          itemBuilder: (context, index) {
+            final challenge = challenges[index];
+            return ExpansionTile(
+              title: Text(challenge.title),
+              trailing: AnimatedCheckIcon(isChecked: challenge.isCompleted),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: challenge.description
+                ),
+                TextButton(
+                  onPressed: () => toggleCompletion(index),
+                  child: Text(
+                    challenge.isCompleted ? 'Mark Incomplete' : 'Mark Complete',
+                  ),
+                ),
+              ],             
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class AnimatedCheckIcon extends StatefulWidget {
+  final bool isChecked;
+
+  const AnimatedCheckIcon({required this.isChecked, Key? key}) : super(key: key);
+
+  @override
+  _AnimatedCheckIconState createState() => _AnimatedCheckIconState();
+}
+
+class _AnimatedCheckIconState extends State<AnimatedCheckIcon>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 400),
+      vsync: this,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant AnimatedCheckIcon oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isChecked != oldWidget.isChecked) {
+      _controller.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: Tween<double>(begin: 0.0, end: 1.0)
+          .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut)),
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: Icon(
+          widget.isChecked ? Icons.check_box : Icons.check_box_outline_blank,
+          key: ValueKey(widget.isChecked),
+          color: widget.isChecked ? Colors.green : null,
+        ),
+      ),
+    );
+  }
+}
+
+
